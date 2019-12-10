2313cf47bc7553a0e6ceb678a8d8304c873bb120
Dmitry <v.i.p.dimak@gmail.com>
2019-12-10 21:58:31 +0200

    added wallet account tests

--- Example/Tests/WalletAccountSpec.swift
+++ Example/Tests/WalletAccountSpec.swift
@@ -10,33 +10,52 @@ import XCTest
 import Quick
 import Nimble
 import AwaitKit
+import KeychainAccess
 @testable import nearclientios
+
+internal class MockAuthService: ExternalAuthService {
+  var urls: [URL] = []
+
+  func openURL(_ url: URL) -> Bool {
+    urls.append(url)
+    return true
+  }
+}
 
 class WalletAccountSpec: QuickSpec {
 
-  //  var windowSpy
-  //  var documentSpy
-  //  var nearFake
   var walletAccount: WalletAccount!
-  let keyStore = InMemoryKeyStore()
+  var keyStore: KeyStore!
   let walletUrl = "http://example.com/wallet"
+  var authService: MockAuthService!
+  var nearFake: Near!
+  var testStorage: Keychain!
 
-  lazy var nearFake = try! Near(config: NearConfig(networkId: "networkId",
-                                                   nodeUrl: URL(string: walletUrl)!,
-                                                   masterAccount: nil,
-                                                   keyPath: nil,
-                                                   helperUrl: nil,
-                                                   initialBalance: nil,
-                                                   providerType: .jsonRPC(URL(string: walletUrl)!),
-                                                   signerType: .inMemory(keyStore),
-                                                   keyStore: keyStore,
-                                                   contractName: "contractId",
-                                                   walletUrl: walletUrl))
 
   override func spec() {
     describe("WalletAccountSpec") {
       beforeEach {
-        self.walletAccount = try! WalletAccount(near: self.nearFake)
+        self.keyStore = InMemoryKeyStore()
+        self.nearFake = try! Near(config: NearConfig(networkId: "networkId",
+                                                     nodeUrl: URL(string: self.walletUrl)!,
+                                                     masterAccount: nil,
+                                                     keyPath: nil,
+                                                     helperUrl: nil,
+                                                     initialBalance: nil,
+                                                     providerType: .jsonRPC(URL(string: self.walletUrl)!),
+                                                     signerType: .inMemory(self.keyStore),
+                                                     keyStore: self.keyStore,
+                                                     contractName: "contractId",
+                                                     walletUrl: self.walletUrl))
+        self.testStorage = Keychain(service: "TEST_WALLET_STORAGE_SERVICE")
+        self.authService = MockAuthService()
+        self.walletAccount = try! WalletAccount(near: self.nearFake,
+                                                storage: self.testStorage,
+                                                authService: self.authService)
+      }
+
+      afterEach {
+        try! self.testStorage.removeAll()
       }
 
       it("not signed in by default") {
@@ -44,64 +63,49 @@ class WalletAccountSpec: QuickSpec {
       }
 
       it("can request sign in") {
-        //        let newUrl;
-        //        windowSpy.mockImplementation(() => ({
-        //            location: {
-        //                href: 'http://example.com/location',
-        //                assign(url) {
-        //                    newUrl = url;
-        //                }
-        //            }
-        //        }));
-
-//        try! await(self.walletAccount.requestSignIn(contractId: "signInContract",
-//                                                    title: "signInTitle",
-//                                                    successUrl: "http://example.com/success",
-//                                                    failureUrl: "http://example.com/fail"))
-//
-//        let accounts = try! await(self.keyStore.getAccounts(networkId: "networkId"))
-//        expect(accounts).to(haveCount(1))
-//        expect(accounts[0]).to(beginWith("pending_key"))
-        //        expect(url.parse(newUrl, true)).toMatchObject({
-        //            protocol: 'http:',
-        //            host: 'example.com',
-        //            query: {
-        //                title: 'signInTitle',
-        //                contract_id: 'signInContract',
-        //                success_url: 'http://example.com/success',
-        //                failure_url: 'http://example.com/fail',
-        //                public_key: (await keyStore.getKey('networkId', accounts[0])).publicKey.toString()
-        //            }
-        //        });
+        do {
+          try await(self.walletAccount.requestSignIn(contractId: "signInContract",
+                                                      title: "signInTitle",
+                                                      successUrl: URL(string: "customscheme://success"),
+                                                      failureUrl: URL(string: "customscheme://fail"),
+                                                      appUrl: URL(string: "customscheme://")))
+          let accounts = try await(self.keyStore.getAccounts(networkId: "networkId"))
+          expect(accounts).to(haveCount(1))
+          expect(accounts[0]).to(beginWith("pending_key"))
+          expect(self.authService.urls).to(haveCount(1))
+          let newUrl = self.authService.urls.last!
+          expect(newUrl.scheme).to(equal("http"))
+          expect(newUrl.host).to(equal("example.com"))
+          let params = newUrl.queryParameters!
+          expect(params["title"]).to(equal("signInTitle"))
+          expect(params["contract_id"]).to(equal("signInContract"))
+          expect(params["success_url"]).to(equal("customscheme://success"))
+          expect(params["failure_url"]).to(equal("customscheme://fail"))
+          let keyPair = try await(self.keyStore.getKey(networkId: "networkId", accountId: accounts[0]))
+          expect(params["public_key"]).to(equal(keyPair?.getPublicKey().toString()))
+        } catch let error {
+          fail("\(error)")
+        }
       }
 
-//      it("can complete sign in") {
-//        let keyPair = try! keyPairFromRandom() as! KeyPairEd25519
-        //        var history = []
-        //        windowSpy.mockImplementation(() => ({
-        //            location: {
-        //                href: `http://example.com/location?account_id=near.account&public_key=${keyPair.publicKey}`
-        //            },
-        //            history: {
-        //                replaceState: (state, title, url) => history.push([state, title, url])
-        //            },
-        //            localStorage
-        //        }))
-        //        documentSpy.mockImplementation(() => ({
-        //            title: 'documentTitle'
-        //        }))
-//        try! await(self.keyStore.setKey(networkId: "networkId",
-//                                        accountId: "pending_key" + keyPair.getPublicKey().toString(),
-//                                        keyPair: keyPair))
-
-//        self.walletAccount._completeSignInWithAccessKey()
-//        let testKeyPair = try! await(self.keyStore.getKey(networkId: "networkId", accountId: "near.account")) as! KeyPairEd25519
-//        expect(testKeyPair).to(equal(keyPair))
-        //        expect(localStorage.getItem('contractId_wallet_auth_key'));
-        //        expect(history).toEqual([
-        //            [{}, 'documentTitle', 'http://example.com/location']
-        //        ]);
-//      }
+      it("can complete sign in") {
+        do {
+          let keyPair = try keyPairFromRandom() as! KeyPairEd25519
+          try await(self.keyStore.setKey(networkId: "networkId",
+                                         accountId: "pending_key" + keyPair.getPublicKey().toString(),
+                                         keyPair: keyPair))
+          let public_key = keyPair.getPublicKey().toString()
+          let url = URL(string: "customscheme://success?account_id=near.account&public_key=\(public_key)")!
+          try await(self.walletAccount.completeSignIn(UIApplication.shared, open: url))
+          let testKeyPair = try await(self.keyStore.getKey(networkId: "networkId",
+                                                           accountId: "near.account"))
+          expect(testKeyPair as? KeyPairEd25519).to(equal(keyPair))
+          expect(self.walletAccount.isSignedIn()).to(beTrue())
+          expect(self.walletAccount.getAccountId()).to(equal("near.account"))
+        } catch let error {
+          fail("\(error)")
+        }
+      }
     }
   }
 }
--- nearclientios/Sources/WalletAccount.swift
+++ nearclientios/Sources/WalletAccount.swift
@@ -39,6 +39,16 @@ internal protocol WalletStorage: class {
   subscript(key: String) -> String? {get set}
 }
 
+let WALLET_STORAGE_SERVICE = "nearlib.wallet"
+
+extension Keychain: WalletStorage {}
+
+internal protocol ExternalAuthService {
+  func openURL(_ url: URL) -> Bool
+}
+
+extension UIApplication: ExternalAuthService {}
+
 internal struct WalletAccount {
   private let _walletBaseUrl: String
   private let _authDataKey: String
@@ -46,21 +56,20 @@ internal struct WalletAccount {
   private var _authData: AuthDataProtocol
   private let _networkId: String
   private let storage: WalletStorage
+  private let authService: ExternalAuthService
 }
-
-let WALLET_STORAGE_SERVICE = "nearlib.wallet"
-
-extension Keychain: WalletStorage {}
 
 extension WalletAccount {
   init(near: Near, appKeyPrefix: String? = nil,
-       storage: WalletStorage = Keychain(service: WALLET_STORAGE_SERVICE)) throws {
+       storage: WalletStorage = Keychain(service: WALLET_STORAGE_SERVICE),
+       authService: ExternalAuthService = UIApplication.shared) throws {
     let keyPrefix = appKeyPrefix ?? (near.config.contractName ?? "default")
     let authDataKey = keyPrefix + LOCAL_STORAGE_KEY_SUFFIX
     guard let keyStore = (near.connection.signer as? InMemorySigner)?.keyStore else {throw WalletAccountError.noKeyStore}
     let authData = AuthData(accountId: storage[authDataKey])
     self.init(_walletBaseUrl: near.config.walletUrl, _authDataKey: authDataKey,
-              _keyStore: keyStore, _authData: authData, _networkId: near.config.networkId, storage: storage)
+              _keyStore: keyStore, _authData: authData, _networkId: near.config.networkId,
+              storage: storage, authService: authService)
   }
 }
 
@@ -79,7 +88,7 @@ extension WalletAccount {
         walletAccount.getAccountId()
     - Returns: Authorized Account ID.
    */
-  private func getAccountId() -> String {
+  func getAccountId() -> String {
       return _authData.accountId ?? ""
   }
 
@@ -92,17 +101,19 @@ extension WalletAccount {
         - failureUrl: failureUrl url to redirect on failure
    */
   func requestSignIn(contractId: String, title: String,
-                     successUrl: URL? = nil, failureUrl: URL? = nil, appUrl: URL? = nil) throws -> Promise<Void> {
-    guard getAccountId().isEmpty else {return .value(())}
-    guard try await(_keyStore.getKey(networkId: _networkId, accountId: getAccountId())) == nil else {return .value(())}
+                     successUrl: URL? = nil, failureUrl: URL? = nil, appUrl: URL? = nil) throws -> Promise<Bool> {
+    guard getAccountId().isEmpty else {return .value(true)}
+    guard try await(_keyStore.getKey(networkId: _networkId, accountId: getAccountId())) == nil else {return .value(true)}
 
     guard let appUrlSchemes = UIApplication.urlSchemes?.compactMap(URL.init(string:)), !appUrlSchemes.isEmpty else {
       throw WalletAccountError.noRegisteredURLSchemes
     }
-    if let successUrlScheme = successUrl?.scheme, appUrlSchemes.map({$0.absoluteString}).filter({$0.hasPrefix(successUrlScheme)}).isEmpty {
+    if let successUrlScheme = successUrl?.scheme, appUrlSchemes.map({$0.absoluteString}).filter({$0.hasPrefix(successUrlScheme)}).isEmpty,
+      appUrl?.scheme != successUrlScheme {
       throw WalletAccountError.successUrlWrongScheme
     }
-    if let failureUrlScheme = failureUrl?.scheme, appUrlSchemes.map({$0.absoluteString}).filter({$0.hasPrefix(failureUrlScheme)}).isEmpty {
+    if let failureUrlScheme = failureUrl?.scheme, appUrlSchemes.map({$0.absoluteString}).filter({$0.hasPrefix(failureUrlScheme)}).isEmpty,
+      appUrl?.scheme != failureUrlScheme {
       throw WalletAccountError.failureUrlWrongScheme
     }
     let firstAppUrlScheme = appUrlSchemes.first!
@@ -123,28 +134,29 @@ extension WalletAccount {
     let accountId = PENDING_ACCESS_KEY_PREFIX + accessKey.getPublicKey().toString()
     try await(_keyStore.setKey(networkId: _networkId, accountId: accountId, keyPair: accessKey))
     if let openUrl = newUrlComponents?.url {
-      UIApplication.shared.openURL(openUrl)
+      return .value(authService.openURL(openUrl))
     }
-    return .value(())
+    return .value(false)
   }
 
   /**
       Complete sign in for a given account id and public key. To be invoked by the app when getting a callback from the wallet.
    */
   mutating func completeSignIn(_ app: UIApplication,
-                      open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) throws {
+                      open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) throws -> Promise<Void> {
     guard let params = url.queryParameters else {throw WalletAccountError.callbackUrlParamsNotValid}
     if let publicKey = params["public_key"], let accountId = params["account_id"] {
       _authData = AuthData(accountId: accountId)
       storage[_authDataKey] = accountId
       try await(_moveKeyFromTempToPermanent(accountId: accountId, publicKey: publicKey))
     }
+    return .value(())
   }
 
   private func _moveKeyFromTempToPermanent(accountId: String, publicKey: String) throws -> Promise<Void> {
-    let accountId = PENDING_ACCESS_KEY_PREFIX + publicKey
+    let pendingAccountId = PENDING_ACCESS_KEY_PREFIX + publicKey
     guard let keyPair = try await(_keyStore.getKey(networkId: _networkId,
-                                                   accountId: accountId)) else {throw WalletAccountError.noKeyPair}
+                                                   accountId: pendingAccountId)) else {throw WalletAccountError.noKeyPair}
     try await(_keyStore.setKey(networkId: _networkId, accountId: accountId, keyPair: keyPair))
     try await(_keyStore.removeKey(networkId: _networkId, accountId: PENDING_ACCESS_KEY_PREFIX + publicKey))
     return .value(())
