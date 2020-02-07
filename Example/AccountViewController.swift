68a070421530c2deed97072faf5d46f65e69b788
Viktor Siruk <sirukviktor@gmail.com>
2020-02-07 09:56:22 +0200

    just added example to sing-in to wallet and fetching account

---
+++ Example/AccountViewController.swift
@@ -0,0 +1,100 @@
+//
+//  AccountViewController.swift
+//  nearclientios_Example
+//
+//  Created by Viktor Siruk on 06.02.2020.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import nearclientios
+import PromiseKit
+import AwaitKit
+
+struct AccountStateField {
+  let title: String
+  let value: String
+}
+
+class AccountViewController: UITableViewController {
+  
+  private var walletAccount: WalletAccount?
+  private var near: Near?
+  private var accountState: AccountState?
+  private var data = [AccountStateField]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    title = "Near account"
+    setupSignOutButton()
+    accountState = fetchAccountState()
+    setupData(with: accountState!)
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
+
+extension AccountViewController {
+  private func setupSignOutButton() {
+    navigationItem.hidesBackButton = true
+    let newBackButton = UIBarButtonItem(title: "Sign out", style: UIBarButtonItem.Style.plain, target: self, action: #selector(AccountViewController.back(sender:)))
+    self.navigationItem.leftBarButtonItem = newBackButton
+  }
+  
+  @objc private func back(sender: UIBarButtonItem) {
+    walletAccount?.signOut()
+    navigationController?.popViewController(animated: true)
+  }
+}
+
+extension AccountViewController {
+  func setup(near: Near, wallet: WalletAccount) {
+    self.near = near
+    walletAccount = wallet
+  }
+  
+  private func fetchAccountState() -> AccountState {
+    let account = try! await(near!.account(accountId: walletAccount!.getAccountId()))
+    return try! await(account.state())
+  }
+  
+  private func setupData(with accountState: AccountState) { 
+    data.append(AccountStateField(title: "Account ID", value: walletAccount!.getAccountId()))
+    let balance = String(accountState.amount.prefix(accountState.amount.count - 24))  //24 near indivisible units
+    data.append(AccountStateField(title: "Balance", value: balance))
+    data.append(AccountStateField(title: "Storage (used/paid)", value: "\(accountState.storage_usage.toStorageUnit())/\(accountState.storage_paid_at.toStorageUnit())"))
+  }
+}
+
+extension AccountViewController {
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return data.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    var cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier")
+    if cell == nil {
+      cell = UITableViewCell(style: .value1, reuseIdentifier: "cellIdentifier")
+    }
+    let field = data[indexPath.row]
+    cell!.textLabel?.text = field.title
+    cell!.detailTextLabel?.text = field.value
+    return cell!
+  }
+}
+
+extension Number {
+  func toStorageUnit() -> String {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .decimal
+    return formatter.string(from: NSNumber(value: self))!
+  }
+}
---
+++ Example/nearclientios/WelcomeViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  nearclientios
+//
+//  Created by dmitrykurochka on 10/28/2019.
+//  Copyright (c) 2019 dmitrykurochka. All rights reserved.
+//
+
+import UIKit
+import nearclientios
+import PromiseKit
+import AwaitKit
+
+protocol WalletSignInDelegate: class {
+  func completeSignIn(_ app: UIApplication,
+                      open url: URL, options: [UIApplication.OpenURLOptionsKey: Any])
+}
+
+class WelcomeViewController: UIViewController {
+  
+  private var walletAccount: WalletAccount?
+  private var near: Near?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    walletAccount = setupWallet()
+    setupUI(with: walletAccount!)
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  private func setupWallet() -> WalletAccount {
+    let keyStore = InMemoryKeyStore()
+    let config = NearConfig(networkId: "default",
+                            nodeUrl: URL(string: "https://rpc.nearprotocol.com")!,
+                            masterAccount: nil,
+                            keyPath: nil,
+                            helperUrl: nil,
+                            initialBalance: nil,
+                            providerType: .jsonRPC(URL(string: "https://rpc.nearprotocol.com")!),
+                            signerType: .inMemory(keyStore),
+                            keyStore: keyStore,
+                            contractName: "myContractId",
+                            walletUrl: "https://wallet.nearprotocol.com")
+    near = try! Near(config: config)
+    return try! WalletAccount(near: near!)
+  }
+  
+  private func setupUI(with wallet: WalletAccount) {
+    if wallet.isSignedIn() {
+     showAccountState(with: wallet)
+    } else {
+      //Hide preloader
+    }
+  }
+  
+  private func showAccountState(with wallet: WalletAccount) {
+    guard let accountVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "AccountViewController") as? AccountViewController else {
+      return
+    }
+    accountVC.setup(near: near!, wallet: wallet)
+    navigationController?.pushViewController(accountVC, animated: true)
+  }
+  
+  @IBAction func tapShowAuthForm(_ sender: UIButton) {
+    let appName = UIApplication.name ?? "signInTitle"
+    (UIApplication.shared.delegate as? AppDelegate)?.walletSignIn = self
+    try! await(walletAccount!.requestSignIn(contractId: "signInContract",
+                                            title: appName,
+                                            successUrl: URL(string: "nearclientios://success"),
+                                            failureUrl: URL(string: "nearclientios://fail"),
+                                            appUrl: URL(string: "nearclientios://")))
+  }
+}
+
+extension WelcomeViewController: WalletSignInDelegate {
+  func completeSignIn(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) {
+    do {
+      try await(walletAccount!.completeSignIn(app, open: url, options: options))
+    } catch {
+      let alert = UIAlertController(title: "Error", message: "\(error)", preferredStyle: .alert)
+      present(alert, animated: true, completion: nil)
+    }
+    setupUI(with: walletAccount!)
+  }
+}
+
