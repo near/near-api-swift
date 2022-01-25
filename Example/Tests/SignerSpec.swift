3eeb209d14752f9a5ed6ef75ae88b97c35f4c394
Sam Ingle <sam@untoldhq.com>
2022-01-25 14:31:20 -0800

    SignerSpec restored

--- Example/Tests/SignerSpec.swift
+++ Example/Tests/SignerSpec.swift
@@ -1,22 +1,36 @@
-////
-////  SignerSpec.swift
-////  nearclientios_Tests
-////
-////  Created by Dmytro Kurochka on 28.11.2019.
-////  Copyright © 2019 CocoaPods. All rights reserved.
-////
 //
-//import XCTest
-//@testable import nearclientios
+//  SignerSpec.swift
+//  nearclientios_Tests
 //
-//class SignerSpec: QuickSpec {
+//  Created by Dmytro Kurochka on 28.11.2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
 //
-//  override func spec() {
-//    describe("SignerSpec") {
-//      it("it should throw no key") {
-//        let signer = InMemorySigner(keyStore: InMemoryKeyStore())
-//        try! expect(signer.signMessage(message: "message".baseDecoded, accountId: "user", networkId: "network")).to(throwError(errorType: InMemorySignerError.self))
-//      }
-//    }
-//  }
-//}
+
+import XCTest
+@testable import nearclientios
+
+class SignerSpec: XCTestCase {
+  func testNoKeyThrowsError() async throws {
+    let signer = InMemorySigner(keyStore: InMemoryKeyStore())
+    await XCTAssertThrowsError(try await signer.signMessage(message: "message".baseDecoded, accountId: "user", networkId: "network")) { error in
+      XCTAssertTrue(error is InMemorySignerError)
+    }
+  }
+}
+
+extension XCTest {
+    func XCTAssertThrowsError(
+        _ expression: @autoclosure () async throws -> Any,
+        _ message: @autoclosure () -> String = "",
+        file: StaticString = #filePath,
+        line: UInt = #line,
+        _ errorHandler: (_ error: Error) -> Void = { _ in }
+    ) async {
+        do {
+            _ = try await expression()
+            XCTFail(message(), file: file, line: line)
+        } catch {
+            errorHandler(error)
+        }
+    }
+}
