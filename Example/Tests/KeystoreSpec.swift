fcbf977b773183adc1fba30d343b858ba8c70f1d
Sam Ingle <sam@untoldhq.com>
2022-01-26 11:25:42 -0800

    KeyStore tests restored

--- Example/Tests/KeystoreSpec.swift
+++ Example/Tests/KeystoreSpec.swift
@@ -1,78 +1,113 @@
-////
-////  KeystoreSpec.swift
-////  nearclientios
-////
-////  Created by Dmytro Kurochka on 26.11.2019.
-////  Copyright © 2019 CocoaPods. All rights reserved.
-////
 //
-//import XCTest
-//@testable import nearclientios
+//  KeystoreSpec.swift
+//  nearclientios
 //
-//let NETWORK_ID_SINGLE_KEY = "singlekeynetworkid"
-//let ACCOUNT_ID_SINGLE_KEY = "singlekey_accountid"
-//let secretKey = "2wyRcSwSuHtRVmkMCGjPwnzZmQLeXLzLLyED1NDMt4BjnKgQL6tF85yBx6Jr26D2dUNeC716RBoTxntVHsegogYw"
-//let KEYPAIR_SINGLE_KEY = try! KeyPairEd25519(secretKey: secretKey)
+//  Created by Dmytro Kurochka on 26.11.2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
 //
-//class KeyStoreSpec: Behavior<KeyStore> {
-//  override class func spec(_ context: @escaping () -> KeyStore) {
-//    let keyStore = context()
-//
-//    describe("Should store and retrieve keys") {
-//
-//      beforeEach {
-//        try! await(keyStore.setKey(networkId: NETWORK_ID_SINGLE_KEY,
-//                                   accountId: ACCOUNT_ID_SINGLE_KEY,
-//                                   keyPair: KEYPAIR_SINGLE_KEY))
-//      }
-//
-//      afterEach {
-//        try! await(keyStore.clear())
-//      }
-//
-//      it("Get all keys with empty network returns empty list") {
-//        let emptyList = try! await(keyStore.getAccounts(networkId: "emptynetwork"))
-//        expect(emptyList.count).to(equal(0))
-//      }
-//
-//      it("Get all keys with single key in keystore") {
-//        let accountIds = try! await(keyStore.getAccounts(networkId: NETWORK_ID_SINGLE_KEY))
-//        expect(accountIds).to(equal([ACCOUNT_ID_SINGLE_KEY]))
-//      }
-//
-//      it("Get not-existing account") {
-//        let account = try! await(keyStore.getKey(networkId: "somenetwork", accountId: "someaccount"))
-//        expect(account).to(beNil())
-//      }
-//
-//      it("Get account id from a network with single key") {
-//        let key = try! await(keyStore.getKey(networkId: NETWORK_ID_SINGLE_KEY,
-//                                             accountId: ACCOUNT_ID_SINGLE_KEY)) as? KeyPairEd25519
-//        expect(key).to(equal(KEYPAIR_SINGLE_KEY))
-//      }
-//
-//      it("Get networks") {
-//        let networks = try! await(keyStore.getNetworks())
-//        expect(networks).to(equal([NETWORK_ID_SINGLE_KEY]))
-//      }
-//
-//      it("Add two keys to network and retrieve them") {
-//        let networkId = "twoKeyNetwork"
-//        let accountId1 = "acc1"
-//        let accountId2 = "acc2"
-//        let key1Expected = try! keyPairFromRandom() as! KeyPairEd25519
-//        let key2Expected = try! keyPairFromRandom() as! KeyPairEd25519
-//        try! await(keyStore.setKey(networkId: networkId, accountId: accountId1, keyPair: key1Expected))
-//        try! await(keyStore.setKey(networkId: networkId, accountId: accountId2, keyPair: key2Expected))
-//        let key1 = try! await(keyStore.getKey(networkId: networkId, accountId: accountId1)) as! KeyPairEd25519
-//        let key2 = try! await(keyStore.getKey(networkId: networkId, accountId: accountId2)) as! KeyPairEd25519
-//        expect(key1).to(equal(key1Expected))
-//        expect(key2).to(equal(key2Expected))
-//        let accountIds = try! await(keyStore.getAccounts(networkId: networkId))
-//        expect(accountIds.sorted()).to(equal([accountId1, accountId2].sorted()))
-//        let networks = try! await(keyStore.getNetworks())
-//        expect(networks.sorted()).to(equal([NETWORK_ID_SINGLE_KEY, networkId].sorted()))
-//      }
-//    }
-//  }
-//}
+
+import XCTest
+@testable import nearclientios
+
+let NETWORK_ID_SINGLE_KEY = "singlekeynetworkid"
+let ACCOUNT_ID_SINGLE_KEY = "singlekey_accountid"
+let secretKey = "2wyRcSwSuHtRVmkMCGjPwnzZmQLeXLzLLyED1NDMt4BjnKgQL6tF85yBx6Jr26D2dUNeC716RBoTxntVHsegogYw"
+let KEYPAIR_SINGLE_KEY = try! KeyPairEd25519(secretKey: secretKey)
+
+class KeyStoreSpec: XCTestCase {
+  static var keyStores: [KeyStore] = []
+  override class func setUp() {
+    keyStores.append(InMemoryKeyStore())
+    keyStores.append(UnencryptedFileSystemKeyStore(keyDir: "test-keys"))
+    keyStores.append(KeychainKeyStore(keychain: .init(service: "test.keystore")))
+    keyStores.append(MergeKeyStore(keyStores: [InMemoryKeyStore(), InMemoryKeyStore()]))
+  }
+  
+  func withAllKeyStores(run: (_: KeyStore) async throws -> Void) async throws {
+    for store in KeyStoreSpec.keyStores {
+      try await run(store)
+    }
+  }
+  
+  override func setUp() async throws {
+    try await withAllKeyStores(run: setUp(keyStore:))
+  }
+  
+  func setUp(keyStore: KeyStore) async throws {
+    try! await(keyStore.setKey(networkId: NETWORK_ID_SINGLE_KEY,
+                               accountId: ACCOUNT_ID_SINGLE_KEY,
+                               keyPair: KEYPAIR_SINGLE_KEY))
+  }
+  
+  override func tearDown() async throws {
+    try await withAllKeyStores(run: tearDown(keyStore:))
+  }
+  
+  func tearDown(keyStore: KeyStore) async throws {
+    try! await(keyStore.clear())
+  }
+  
+  func testGetAllKeysWithEmptyNetworkReturnsEmptyList() async throws {
+    try await withAllKeyStores(run: getAllKeysWithEmptyNetworkReturnsEmptyList)
+  }
+  func getAllKeysWithEmptyNetworkReturnsEmptyList(keyStore: KeyStore) async throws {
+    let emptyList = try! await keyStore.getAccounts(networkId: "emptynetwork")
+    XCTAssertEqual(emptyList.count, 0)
+  }
+  
+  func testGetAllKeysWithSingleKeyInKeyStore() async throws {
+    try await withAllKeyStores(run: getAllKeysWithSingleKeyInKeyStore)
+  }
+  func getAllKeysWithSingleKeyInKeyStore(keyStore: KeyStore) async throws {
+    let accountIds = try! await keyStore.getAccounts(networkId: NETWORK_ID_SINGLE_KEY)
+    XCTAssertEqual(accountIds, [ACCOUNT_ID_SINGLE_KEY])
+  }
+  
+  func testGetNonExistingAccount() async throws {
+    try await withAllKeyStores(run: getNonExistingAccount)
+  }
+  func getNonExistingAccount(keyStore: KeyStore) async throws {
+    let account = try! await(keyStore.getKey(networkId: "somenetwork", accountId: "someaccount"))
+    XCTAssertNil(account)
+  }
+  
+  func testGetAccountIdFromNetworkWithSingleKey() async throws {
+    try await withAllKeyStores(run: getAccountIdFromNetworkWithSingleKey)
+  }
+  func getAccountIdFromNetworkWithSingleKey(keyStore: KeyStore) async throws {
+    let key = try! await keyStore.getKey(networkId: NETWORK_ID_SINGLE_KEY,
+                                         accountId: ACCOUNT_ID_SINGLE_KEY) as? KeyPairEd25519
+    XCTAssertEqual(key, KEYPAIR_SINGLE_KEY)
+  }
+
+  func testGetNetworks() async throws {
+    try await withAllKeyStores(run: getNetworks)
+  }
+  func getNetworks(keyStore: KeyStore) async throws {
+    let networks = try! await keyStore.getNetworks()
+    XCTAssertEqual(networks, [NETWORK_ID_SINGLE_KEY])
+  }
+  
+  func testAddTwoKeysToNetworkAndRetrieveThem() async throws {
+    try await withAllKeyStores(run: addTwoKeysToNetworkAndRetrieveThem)
+  }
+  func addTwoKeysToNetworkAndRetrieveThem(keyStore: KeyStore) async throws {
+    let networkId = "twoKeyNetwork"
+    let accountId1 = "acc1"
+    let accountId2 = "acc2"
+    let key1Expected = try! keyPairFromRandom() as! KeyPairEd25519
+    let key2Expected = try! keyPairFromRandom() as! KeyPairEd25519
+    try! await keyStore.setKey(networkId: networkId, accountId: accountId1, keyPair: key1Expected)
+    try! await keyStore.setKey(networkId: networkId, accountId: accountId2, keyPair: key2Expected)
+    let key1 = try! await keyStore.getKey(networkId: networkId, accountId: accountId1) as! KeyPairEd25519
+    let key2 = try! await keyStore.getKey(networkId: networkId, accountId: accountId2) as! KeyPairEd25519
+    XCTAssertEqual(key1, key1Expected)
+    XCTAssertEqual(key2, key2Expected)
+    let accountIds = try! await keyStore.getAccounts(networkId: networkId)
+    XCTAssertEqual(accountIds.sorted(), [accountId1, accountId2].sorted())
+    let networks = try! await(keyStore.getNetworks())
+    XCTAssertEqual(networks.sorted(), [NETWORK_ID_SINGLE_KEY, networkId].sorted())
+  }
+
+}
+
--- Example/Tests/MergeKeyStoreSpec.swift
+++ Example/Tests/MergeKeyStoreSpec.swift
@@ -1,52 +1,44 @@
+
+//  MergeKeyStoreSpec.swift
+//  nearclientios_Tests
 //
-////  MergeKeyStoreSpec.swift
-////  nearclientios_Tests
-////
-////  Created by Dmytro Kurochka on 26.11.2019.
-////  Copyright © 2019 CocoaPods. All rights reserved.
-////
+//  Created by Dmytro Kurochka on 26.11.2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
 //
-//import XCTest
-//@testable import nearclientios
-//
-//class MergeKeyStoreSpec: QuickSpec {
-//  static let keystoreService = "test.keystore"
-//
-//  private let stores: [KeyStore] = [InMemoryKeyStore(), InMemoryKeyStore()]
-//  private lazy var keyStore: KeyStore! = MergeKeyStore(keyStores: stores)
-//
-//  override func spec() {
-//
-//    afterEach {
-//      try! await(self.keyStore.clear())
-//    }
-//
-//    it("looks up key from fallback key store if needed") {
-//      let key1 = try! keyPairFromRandom() as! KeyPairEd25519
-//      try! await(self.stores[1].setKey(networkId: "network", accountId: "account", keyPair: key1))
-//      let key = try! await(self.keyStore.getKey(networkId: "network", accountId: "account")) as! KeyPairEd25519
-//      expect(key).to(equal(key1))
-//    }
-//
-//    it("looks up key in proper order") {
-//      let key1 = try! keyPairFromRandom() as! KeyPairEd25519
-//      let key2 = try! keyPairFromRandom() as! KeyPairEd25519
-//      try! await(self.stores[0].setKey(networkId: "network", accountId: "account", keyPair: key1))
-//      try! await(self.stores[1].setKey(networkId: "network", accountId: "account", keyPair: key2))
-//      let key = try! await(self.keyStore.getKey(networkId: "network", accountId: "account")) as! KeyPairEd25519
-//      expect(key).to(equal(key1))
-//    }
-//
-//    it("sets keys only in first key store") {
-//      let key1 = try! keyPairFromRandom() as! KeyPairEd25519
-//      try! await(self.keyStore.setKey(networkId: "network", accountId: "account", keyPair: key1))
-//      let account1 = try! await(self.stores[0].getAccounts(networkId: "network"))
-//      let account2 = try! await(self.stores[1].getAccounts(networkId: "network"))
-//      expect(account1).to(haveCount(1))
-//      expect(account2).to(haveCount(0))
-//    }
-//
-//    itBehavesLike(KeyStoreSpec.self) {self.keyStore}
-//  }
-//}
-//
+
+import XCTest
+@testable import nearclientios
+
+class MergeKeyStoreSpec: XCTestCase {
+  private let stores: [KeyStore] = [InMemoryKeyStore(), InMemoryKeyStore()]
+  private lazy var keyStore: KeyStore! = MergeKeyStore(keyStores: stores)
+  
+  override func tearDown() async throws {
+    try! await(self.keyStore.clear())
+  }
+  
+  func testLookUpKeyFromFallbackKeystoreIfNeeded() async throws {
+    let key1 = try! keyPairFromRandom() as! KeyPairEd25519
+    try! await self.stores[1].setKey(networkId: "network", accountId: "account", keyPair: key1)
+    let key = try! await self.keyStore.getKey(networkId: "network", accountId: "account") as! KeyPairEd25519
+    XCTAssertEqual(key, key1)
+  }
+  
+  func testKeyLookupOrder() async throws {
+    let key1 = try! keyPairFromRandom() as! KeyPairEd25519
+    let key2 = try! keyPairFromRandom() as! KeyPairEd25519
+    try! await self.stores[0].setKey(networkId: "network", accountId: "account", keyPair: key1)
+    try! await self.stores[1].setKey(networkId: "network", accountId: "account", keyPair: key2)
+    let key = try! await self.keyStore.getKey(networkId: "network", accountId: "account") as! KeyPairEd25519
+    XCTAssertEqual(key, key1)
+  }
+  
+  func testSetsKeysOnlyInFirstKeyStore() async throws {
+    let key1 = try! keyPairFromRandom() as! KeyPairEd25519
+    try! await self.keyStore.setKey(networkId: "network", accountId: "account", keyPair: key1)
+    let account1 = try! await self.stores[0].getAccounts(networkId: "network")
+    let account2 = try! await self.stores[1].getAccounts(networkId: "network")
+    XCTAssertEqual(account1.count, 1)
+    XCTAssertEqual(account2.count, 0)
+  }
+}
