943dd3247d9b33a9aa76f6707b2d15719649a49d
Nat McConnaughay <natmcconnaughay@gmail.com>
2022-01-27 12:47:57 -0500

    Get ProviderSpec tests working

--- Example/Tests/ProviderSpec.swift
+++ Example/Tests/ProviderSpec.swift
@@ -1,94 +1,88 @@
-////
-////  ProviderSpec.swift
-////  nearclientios_Tests
-////
-////  Created by Dmytro Kurochka on 27.11.2019.
-////  Copyright © 2019 CocoaPods. All rights reserved.
-////
 //
-//import XCTest
-//@testable import nearclientios
+//  ProviderSpec.swift
+//  nearclientios_Tests
 //
-//class ProviderSpec: QuickSpec {
-//  private var provider: Provider!
+//  Created by Dmytro Kurochka on 27.11.2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
 //
-//  override func spec() {
-//    describe("ProviderSpec") {
-//
-//      beforeEach {
-//        let url = getConfig(env: .ci).nodeUrl
-//        self.provider = JSONRPCProvider(url: url)
-//      }
-//
-////      it("should fetch node status") {
-////        let response = try! await(self.provider.status())
-////        expect(response.chain_id).to(contain("test-chain"))
-////      }
-//
-////      it("should fetch block info") {
-////        let response = try! await(provider.block(blockId: 1))
-////        expect(response.header.height).to(equal(1))
-////        let sameBlock = try! await(provider.block(response.header.hash))
-////        expect(sameBlock.header.height).to(equal(1))
-////      }
-////
-////      it("should fetch chunk info") {
-////        let response = try! await(provider.chunk(chunkId: [1, 0]))
-////        expect(response.header.shard_id).to(equal(0))
-////        let sameChunk = try! await(provider.chunk(response.header.chunk_hash))
-////        expect(sameChunk.header.chunk_hash).to(equal(response.header.chunk_hash))
-////        expect(sameChunk.header.shard_id).to(equal(0))
-////      }
-////
-////      it("should query account") {
-////        let response = try! await(provider.query("account/test.near", ""))
-////        expect(response.code_hash).to(equal("11111111111111111111111111111111"))
-////      }
-//
-//      it("should have correct final tx result") {
-//        let outcome = ExecutionOutcome(status: .successReceiptId("11112"),
-//                                       logs: [],
-//                                       receipt_ids: ["11112"],
-//                                       gas_burnt: 1)
-//        let transaction = ExecutionOutcomeWithId(id: "11111", outcome: outcome)
-//        let firstRecipientOutcome = ExecutionOutcome(status: .successValue("e30="),
-//                                                     logs: [],
-//                                                     receipt_ids: ["11112"],
-//                                                     gas_burnt: 9001)
-//        let secondRecipientOutcome = ExecutionOutcome(status: .successValue(""),
-//                                                      logs: [],
-//                                                      receipt_ids: [],
-//                                                      gas_burnt: 0)
-//        let receipts = [ExecutionOutcomeWithId(id: "11112", outcome: firstRecipientOutcome),
-//                        ExecutionOutcomeWithId(id: "11113", outcome: secondRecipientOutcome)]
-//        let result = FinalExecutionOutcome(status: .successValue("e30="),
-//                                           transaction: transaction,
-//                                           receipts: receipts)
-//        expect(getTransactionLastResult(txResult: result)).notTo(beNil())
-//      }
-//
-//      it("should have final tx result with nil") {
-//        let outcome = ExecutionOutcome(status: .successReceiptId("11112"),
-//                                       logs: [],
-//                                       receipt_ids: ["11112"],
-//                                       gas_burnt: 1)
-//        let transaction = ExecutionOutcomeWithId(id: "11111", outcome: outcome)
-//        let firstRecipientOutcome = ExecutionOutcome(status: .failure(ExecutionError()),
-//                                                     logs: [],
-//                                                     receipt_ids: ["11112"],
-//                                                     gas_burnt: 9001)
-//        let secondRecipientOutcome = ExecutionOutcome(status: .successValue(""),
-//                                                      logs: [],
-//                                                      receipt_ids: [],
-//                                                      gas_burnt: 0)
-//        let receipts = [ExecutionOutcomeWithId(id: "11112", outcome: firstRecipientOutcome),
-//                        ExecutionOutcomeWithId(id: "11113", outcome: secondRecipientOutcome)]
-//        let result = FinalExecutionOutcome(status: .failure(ExecutionError()),
-//                                           transaction: transaction,
-//                                           receipts: receipts)
-//        expect(getTransactionLastResult(txResult: result)).to(beNil())
-//      }
-//    }
+
+import XCTest
+@testable import nearclientios
+
+class ProviderSpec: XCTestCase {
+  private var provider: Provider!
+  override func setUp() {
+    let url = getConfig(env: .ci).nodeUrl
+    self.provider = JSONRPCProvider(url: url)
+  }
+  
+  func testFetchNodeStatus() async {
+    let response = try! await self.provider.status()
+    XCTAssertTrue(response.chain_id.contains("ci-testnet"))
+  }
+  
+  func testCorrectFinalTransactionResult() {
+    let outcome = ExecutionOutcome(status: .successReceiptId("11112"),
+                                   logs: [],
+                                   receipt_ids: ["11112"],
+                                   gas_burnt: 1)
+    let transaction = ExecutionOutcomeWithId(id: "11111", outcome: outcome)
+    let firstRecipientOutcome = ExecutionOutcome(status: .successValue("e30="),
+                                                 logs: [],
+                                                 receipt_ids: ["11112"],
+                                                 gas_burnt: 9001)
+    let secondRecipientOutcome = ExecutionOutcome(status: .successValue(""),
+                                                  logs: [],
+                                                  receipt_ids: [],
+                                                  gas_burnt: 0)
+    let receipts = [ExecutionOutcomeWithId(id: "11112", outcome: firstRecipientOutcome),
+                    ExecutionOutcomeWithId(id: "11113", outcome: secondRecipientOutcome)]
+    let result = FinalExecutionOutcome(status: .successValue("e30="),
+                                       transaction: transaction,
+                                       receipts: receipts)
+    XCTAssertNotNil(getTransactionLastResult(txResult: result))
+  }
+  
+  func testFinalTransactionResultWithNil() {
+    let outcome = ExecutionOutcome(status: .successReceiptId("11112"),
+                                   logs: [],
+                                   receipt_ids: ["11112"],
+                                   gas_burnt: 1)
+    let transaction = ExecutionOutcomeWithId(id: "11111", outcome: outcome)
+    let firstRecipientOutcome = ExecutionOutcome(status: .failure(ExecutionError()),
+                                                 logs: [],
+                                                 receipt_ids: ["11112"],
+                                                 gas_burnt: 9001)
+    let secondRecipientOutcome = ExecutionOutcome(status: .successValue(""),
+                                                  logs: [],
+                                                  receipt_ids: [],
+                                                  gas_burnt: 0)
+    let receipts = [ExecutionOutcomeWithId(id: "11112", outcome: firstRecipientOutcome),
+                    ExecutionOutcomeWithId(id: "11113", outcome: secondRecipientOutcome)]
+    let result = FinalExecutionOutcome(status: .failure(ExecutionError()),
+                                       transaction: transaction,
+                                       receipts: receipts)
+    XCTAssertNil(getTransactionLastResult(txResult: result))
+  }
+  
+//  func testFetchBlockInfo() async {
+//    let response = try! await provider.block(blockId: "1")
+//    XCTAssertEqual(response.header.height, 1)
+//    let sameBlock = try! await provider.block(blockId: response.header.hash)
+//    XCTAssertEqual(sameBlock.header.height, 1)
 //  }
-//}
-//
+  
+//  func testFetchChunkInfo() async{
+//    let response = try! await provider.chunk(chunkId: "[1, 0]")
+//    XCTAssertEqual(response.header.shard_id, 0)
+//    let sameChunk = try! await provider.chunk(chunkId: response.header.chunk_hash)
+//    XCTAssertEqual(sameChunk.header.chunk_hash, response.header.chunk_hash)
+//    XCTAssertEqual(sameChunk.header.shard_id, 0)
+//  }
+  
+//  func testQueryAccount() async{
+//    let response = try! await provider.query(path: "account/test.near", data: "")
+//    XCTAssertEqual(response.code_hash, "11111111111111111111111111111111")
+//  }
+
+}
