cf35e09080e08bedb5fd8d4caf2c9f74546dff0a
Sam Ingle <sam@untoldhq.com>
2022-05-02 13:23:45 -0700

    1.0.16 Make borsh encoding/decoding public

--- nearclientios/Sources/Utils/Borsh/BorshEncoder.swift
+++ nearclientios/Sources/Utils/Borsh/BorshEncoder.swift
@@ -7,8 +7,9 @@
 
 import Foundation
 
-struct BorshEncoder {
-  func encode<T>(_ value: T) throws -> Data where T : BorshSerializable {
+public struct BorshEncoder {
+  public init() {}
+  public func encode<T>(_ value: T) throws -> Data where T : BorshSerializable {
     var writer = Data()
     try value.serialize(to: &writer)
     return writer
